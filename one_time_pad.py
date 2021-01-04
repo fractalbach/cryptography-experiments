9ac69128aa806fa21b2a76696b791598ffd8328c
fractalbach <fractalbach@gmail.com>
2021-01-04 00:37:50 -0800

    added some bit converting experiments

--- one_time_pad.py
+++ one_time_pad.py
@@ -15,8 +15,21 @@ class OneTimePad:
     def key_gen(self, l):
         return secrets.randbits(l)
 
+def binstr(s):
+    return int(''.join(format(ord(x), 'b') for x in s), 2)
+
+def unbinstr(binstr):
+    return binstr.decode('utf-8')
+
 if __name__ == "__main__":
-    msg = "hello"
     pad = OneTimePad()
-    k = pad.key_gen(10)
-    print(bin(k))
+    msg = "hello"
+    bmsg = binstr(msg)
+    k = pad.key_gen(bmsg.bit_length())
+
+    print(f'message = "{msg}"')
+    print("converting message to binary...")
+    print(f'msg[{bmsg.bit_length()}]', bin(bmsg))
+    print("generating key of the same length...")
+    print(f'key[{k.bit_length()}]', bin(k))
+
